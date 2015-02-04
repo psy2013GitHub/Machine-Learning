d700c6b647bfec3435bea0123ccf0ef1afae7e7b
ZhouDeng <superdengzhou@163.com>
2015-02-04 19:33:33 +0800

    gmm accompleshed

--- ML_lib/EM/GMM/gmm.py
+++ ML_lib/EM/GMM/gmm.py
@@ -1,8 +1,6 @@
 
+from __future__ import division
 import numpy as np
-from future import division
-
-
 
 class GMM(object):
       def __init__(self, thresh_Q = 0.00001):
@@ -10,64 +8,98 @@ class GMM(object):
 
       def init_para(self):
           self.alpha = np.zeros(self.nModels, dtype=np.float)          
+          self.alpha[0:self.nModels-1] = np.random.uniform(0.001, 1/self.nModels, self.nModels-1); self.alpha[self.nModels-1] = 1 - np.sum(self.alpha[0:self.nModels-1])
           self.mv    = np.zeros(self.nModels, dtype=np.float)          
-          self.sigma = np.zeros(self.nModels, dtype=np.float)          
+          self.sigma2 = np.zeros(self.nModels, dtype=np.float) + 1 # sigma squared
           self.E_gamma = np.zeros((self.nSamples, self.nModels), dtype=np.float)
           
-      def E_step_E_gamma(self):
+      def E_step(self):
           for j in xrange(self.nSamples):
-             sum_E_gamma = 0.0
              for k in xrange(self.nModels):
-                self.E_gamma[j, k] = self.alpha[k] * Gaussian_p(self.mv[k], self.sigma[k], self.y[j])  
-                sum_E_gamma += self.E_gamma[j, k]
-             self.E_gamma[j, k] /= sum_E_gamma 
+                p = Gaussian_p(self.mv[k], self.sigma2[k], self.y[j])
+                #print "p, ", p
+                self.E_gamma[j, k] = self.alpha[k] * p  
+             sum_E_gamma = np.sum(self.E_gamma[j, :])
+             self.E_gamma[j, :] = self.E_gamma[j, :] / sum_E_gamma
+          self.E_Q = self.Q_func_val()
 
       def M_step(self):
-          alpha_new = np.zeros(self.nModels, dtype=np.float); nv_new = np.zeros(self.nModels, dtype=np.float); sigma_new = np.zeros(self.nModels, dtype=np.float)
+          # init alpha, mv, sigma
+          alpha_new = np.zeros(self.nModels, dtype=np.float)
+          mv_new = np.zeros(self.nModels, dtype=np.float)
+          sigma2_new = np.zeros(self.nModels, dtype=np.float)
+          Q = 0.0
+          E_gamma_across_samples = np.sum(self.E_gamma, axis=0)
+          for k in xrange(self.nModels):
+             alpha_new[k]  = np.sum(self.E_gamma[:, k]) 
+             mv_new[k]     = np.sum(np.dot(self.E_gamma[:, k], self.y)) 
+             sigma2_new[k]  = np.sum(np.dot(self.E_gamma[:, k], np.power(self.y - self.mv[k], 2)))
+          alpha_new /= self.nSamples
+          mv_new    /= E_gamma_across_samples
+          sigma2_new /= E_gamma_across_samples
+          self.last_alpha = self.alpha
+          self.alpha = alpha_new
+          self.last_mv = self.mv
+          self.mv    = mv_new
+          self.last_sigma2 = self.sigma2
+          self.sigma2 = sigma2_new
+          self.M_Q = self.Q_func_val()
+     
+      def Q_func_val(self):
+          Q = 0.0
           for k in xrange(self.nModels): 
              for j in xrange(self.nSamples):
-               alpha_new[k] += self.E_gamma[j, k] 
-               mv_new[k]    += self.E_gamma[j, k] * self.y[j] 
-               sigma_new[k] += self.E_gamma[j, k] * np.power(self.y[j] - self.mv[k], 2)
-         E_gamma_across_models = np.sum(self.E_gamma, axis=0)
-         alpha_new /= self.nSamples
-         mv_new    /= E_gamma_across_models
-         sigma_new /= E_gamma_across_models
-         self.alpha = alpha_new
-         self.mv    = mv_new
-         self.sigma = sigma_new
- 
-      def E_step_Q_func_value(self):
-          Q = 0.0
-          for j in xrange(self.nSamples):
-             for k in xrange(self.nModels):
-                Q += self.E_sigma[j, k] * np.log(self.alpha[k]) + self.E_sigma[j, k] * np.log(Gaussian_p(self.mv[k], self.sigma[k], self.y[j])) 
+               Q += self.E_gamma[j, k] * np.log(self.alpha[k] + 1) + self.E_gamma[j, k] * np.log(Gaussian_p(self.mv[k], self.sigma2[k], self.y[j]) + 1)
           return Q
 
-      def fit(self, train_X):
-          self.y = self.train_X
-          sef.nSamples, self.nModels = np.shape(train_X)
+      def fit(self, train_X, nModels):
+          self.y = train_X
+          self.nSamples = np.shape(train_X)[0]; self.nModels = nModels
           self.init_para()
-          self.E_step_E_gamma()
-          last_Q = self.E_step_Q_func_value()
-          Q = None
-          while not Q or (Q - last_Q > self.thresh_Q):
+          itera = 0
+          while 1:
+             itera += 1
              self.E_step()
              self.M_step()
-             Q = self.E_step_Q_func_value()
+             # # print self.E_Q, self.M_Q
+             # print "M_Q: ", self.M_Q, "E_Q: ", self.E_Q
+             if itera > 10000: # or np.abs(self.M_Q - self.E_Q) <= self.thresh_Q: # EM is very easily converge to local maximum
+                self.itera = itera
+                break
              
-             
-def Gaussian_p(mv, sigma, y):
-    p = np.exp(-1 * (y - mv) * (y - mv) / (2 * np.power(sigma, 2))) / (np.sqrt(2 * np.pi) * sigma)
+def Gaussian_p(mv, sigma2, y):
+    p = np.exp(-1 * np.power(y - mv, 2) / (2 * sigma2)) / np.sqrt(2 * np.pi * sigma2)
+    p += np.spacing(1) # in case of p approxiamtes 0
+    # print mv, sigma, y, p
     return p
 		
 
 if __name__ == "__main__":
-  # 20 from N(1,4); 20 from N(2,9); 20 from N(13,16)
-  data1 = np.random.randn(20) * 2 + 1
-  data2 = np.random.randn(20) * 3 + 2
-  data3 = np.random.randn(20) * 4 + 13
-  data = np.hstack((data1, data2, data3))
+  # 20 from N(1,4); 20 from N(10,9); 20 from N(30,16)
+  data = []
+  for j in xrange(600):
+     m = np.random.choice(3)
+     if m == 0:
+         num = np.random.randn(1) * 2 + 1
+     elif m == 1:
+         num = np.random.randn(1) * 3 + 10
+     else:
+         num = np.random.randn(1) * 4 + 30
+     data.append(num)
+  data = np.array(data, dtype=np.float)
+  print data.shape
   gmm = GMM()
-  gmm.fit(data) 
-  print self.alpha
+  gmm.fit(data, 3)
+  print "new\n"
+  print gmm.alpha
+  print gmm.mv
+  print gmm.sigma2
+  print "last\n"
+  print gmm.last_alpha
+  print gmm.last_mv
+  print gmm.last_sigma2
+  print gmm.E_Q
+  print gmm.M_Q
+
+
+  print gmm.itera
